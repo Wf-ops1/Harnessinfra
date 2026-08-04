79af2e353ed0743082355364bb8c3a24d98c90bd
wallacefake123-dotcom <wallacefake123@gmail.com>
2026-08-04 02:14:44 -0300

    Revert "test(ci): add controlled required-check failure"
    This reverts commit d9640ff57d2e29c37f257e6a13c08025e68a69bd.

--- tests/unit/test_ci_required_block.py
+++
@@ -1,5 +0,0 @@
-"""Controlled failure used only to prove the required CI check blocks merge."""
-
-
-def test_controlled_required_check_failure() -> None:
-    raise AssertionError("CONTROLLED F0.6-R2 FAILURE: CI required must block merge")
